156665db77b9233114cf7def89040b2d55754a5b
soorajshah2004-cyber <sooraj.shah2004@gmail.com>
2026-07-05 23:10:16 +0530

    Add script to print natural numbers up to user input
    This script prompts the user for a number and prints all natural numbers up to that number.

---
+++ 01-Loops/natural-numbers.js
@@ -0,0 +1,8 @@
+const prompt = require("prompt-sync")();
+// Ques2) Print natural number upto n.
+
+let n = Number(prompt("Enter a number to print Natutal number upto"));
+for(let i = 1; i<= n; i++){
+    console.log(i);
+
+}

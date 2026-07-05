018feb2c2dc7489885ef1f8b641109f1dfb60942
soorajshah2004-cyber <sooraj.shah2004@gmail.com>
2026-07-05 23:09:12 +0530

    Comment out question prompt in hello-world.js

--- 01-Loops/hello-world.js
+++ 01-Loops/hello-world.js
@@ -1,6 +1,6 @@
 const prompt = require("prompt-sync")();
 
-Ques1) Accept an integer and print hello world n times
+// Ques1) Accept an integer and print hello world n times
 
 let n = Number(prompt("Enter a number to print hello world n times"));
 for(let i =1; i<=n; i++){

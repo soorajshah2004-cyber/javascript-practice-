6249587cf8adc8ed2ed2141e303cb03aaa51fcc2
soorajshah2004-cyber <sooraj.shah2004@gmail.com>
2026-07-05 23:13:08 +0530

    Delete 01-Loops/natural-numbers.js

--- 01-Loops/natural-numbers.js
+++
@@ -1,8 +0,0 @@
-const prompt = require("prompt-sync")();
-// Ques2) Print natural number upto n.
-
-let n = Number(prompt("Enter a number to print Natutal number upto"));
-for(let i = 1; i<= n; i++){
-    console.log(i);
-
-}

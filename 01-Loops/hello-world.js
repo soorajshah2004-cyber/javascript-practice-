c704ab4114e7c14323bfb0185791e60306c15fcd
soorajshah2004-cyber <sooraj.shah2004@gmail.com>
2026-07-05 23:13:20 +0530

    Delete 01-Loops/hello-world.js

--- 01-Loops/hello-world.js
+++
@@ -1,8 +0,0 @@
-const prompt = require("prompt-sync")();
-
-// Ques1) Accept an integer and print hello world n times
-
-let n = Number(prompt("Enter a number to print hello world n times"));
-for(let i =1; i<=n; i++){
-    console.log("Hello World");    
-}
